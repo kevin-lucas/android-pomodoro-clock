5dbb5099ee4420d62455f4e05f2cbcc08629a37a
Kevin Lucas <kevinlucas.tec.info@gmail.com>
2021-10-14 19:39:12 -0300

    Refatorando o código

--- app/src/main/java/br/com/kevincode/pomodoroclock/MainActivity.kt
+++ app/src/main/java/br/com/kevincode/pomodoroclock/MainActivity.kt
@@ -1,81 +1,102 @@
 package br.com.kevincode.pomodoroclock
 
-import android.content.DialogInterface
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
-import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
-import androidx.constraintlayout.widget.ConstraintLayout
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var chTimer: Chronometer
+    private lateinit var session: TextView
+    private lateinit var play: ImageButton
+    private lateinit var stop: ImageButton
+    private lateinit var pause: ImageButton
+
+    var count = 0L
+    var isWorkSession = true
+    var countSession = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val play = findViewById<ImageButton>(R.id.bt_play)
-        val stop = findViewById<ImageButton>(R.id.bt_stop)
-        val pause = findViewById<ImageButton>(R.id.bt_pause)
-        val session = findViewById<TextView>(R.id.tv_session)
-        val chTimer = findViewById<Chronometer>(R.id.ch_timer)
+        play = findViewById<ImageButton>(R.id.bt_play)
+        stop = findViewById<ImageButton>(R.id.bt_stop)
+        pause = findViewById<ImageButton>(R.id.bt_pause)
+        session = findViewById<TextView>(R.id.tv_session)
+        chTimer = findViewById<Chronometer>(R.id.ch_timer)
 
-        var count = 0L
-        var isWorkSession = true
-        var countSession = 1
+        initTimer()
+        setOnListenerTimer()
+        setOnClickListenerButtons()
+    }
 
+    private fun initTimer() {
         chTimer.base = SystemClock.elapsedRealtime() + (5 * 60000L)
 
         session.text = "Sessão $countSession"
         chTimer.text = "25:00"
+    }
 
+    private fun setOnClickListenerButtons() {
         play.setOnClickListener {
-            val minutes = if (count > 0) count else 25 * 60000L
-            chTimer.base = SystemClock.elapsedRealtime() + minutes
-            chTimer.isCountDown = true
-            chTimer.start()
-            Toast.makeText(this, getString(R.string.timer_status_started), Toast.LENGTH_SHORT).show()
+            startTimer()
         }
 
         stop.setOnClickListener {
-
-            chTimer.stop()
-
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Resetar Sessão")
-            builder.setMessage("Deseja pular a sessão atual?")
-            builder.setPositiveButton("Sim") { _, _ ->
-
-                if (isWorkSession) {
-                    chTimer.base = SystemClock.elapsedRealtime() + (5 * 60000L)
-                    session.text = "Descanso"
-                    countSession += 1
-                    isWorkSession = false
-
-                } else {
-                    chTimer.base = SystemClock.elapsedRealtime() + (25 * 60000L)
-                    session.text = "Sessão $countSession"
-                    isWorkSession = true
-                }
-
-                Toast.makeText(this, getString(R.string.timer_status_restore), Toast.LENGTH_SHORT).show()
-            }
-            builder.setNegativeButton("Não") { _, _ ->
-                chTimer.start()
-            }
-            builder.show()
+            stopTimer()
         }
 
         pause.setOnClickListener {
-            chTimer.stop()
-            Toast.makeText(this, getString(R.string.timer_status_paused), Toast.LENGTH_SHORT).show()
+            pauseTimer()
         }
+    }
 
+    private fun startTimer() {
+        val minutes = if (count > 0) count else 25 * 60000L
+        chTimer.base = SystemClock.elapsedRealtime() + minutes
+        chTimer.isCountDown = true
+        chTimer.start()
+        Toast.makeText(this, getString(R.string.timer_status_started), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun stopTimer() {
+        chTimer.stop()
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Resetar Sessão")
+        builder.setMessage("Deseja sair da sessão atual?")
+        builder.setPositiveButton("Sim") { _, _ ->
+
+            if (isWorkSession) {
+                chTimer.base = SystemClock.elapsedRealtime() + (5 * 60000L)
+                session.text = "Descanso"
+                countSession += 1
+                isWorkSession = false
+
+            } else {
+                chTimer.base = SystemClock.elapsedRealtime() + (25 * 60000L)
+                session.text = "Sessão $countSession"
+                isWorkSession = true
+            }
+
+            Toast.makeText(this, getString(R.string.timer_status_restore), Toast.LENGTH_SHORT)
+                .show()
+        }
+        builder.setNegativeButton("Não") { _, _ ->
+            chTimer.start()
+        }
+        builder.show()
+    }
+
+    private fun pauseTimer() {
+        chTimer.stop()
+        Toast.makeText(this, getString(R.string.timer_status_paused), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setOnListenerTimer() {
         chTimer.setOnChronometerTickListener {
 
             count = (SystemClock.elapsedRealtime() - chTimer.base) * -1
